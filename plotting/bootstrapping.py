4fb25c013e5db542cf6dbe0d29babc813e067484
michaelBezick <mbezick@purdue.edu>
2025-04-24 15:21:16 -0400

    plots

--- plotting/bootstrapping.py
+++ plotting/bootstrapping.py
@@ -7,13 +7,13 @@ api = wandb.Api()
 
 # plotting 30mm 0 30mm pca
 
-run1 = api.run("/michael-bezick-purdue-university/pprl/runs/5319l3di")
-run2 = api.run("/michael-bezick-purdue-university/pprl/runs/v5qwdkgi")
-run3 = api.run("/michael-bezick-purdue-university/pprl/runs/h786zn4l")
-run4 = api.run("/michael-bezick-purdue-university/pprl/runs/1i5mim3q")
-run5 = api.run("/michael-bezick-purdue-university/pprl/runs/oe84cmx6")
-run6 = api.run("/michael-bezick-purdue-university/pprl/runs/4yk8r4dx")
+run1 = api.run("/michael-bezick-purdue-university/pprl/runs/1vsyf739")
+run2 = api.run("/michael-bezick-purdue-university/pprl/runs/5ovv122x")
+run3 = api.run("/michael-bezick-purdue-university/pprl/runs/dqaqbpqj")
+run4 = api.run("/michael-bezick-purdue-university/pprl/runs/jo7lmavq")
+run5 = api.run("/michael-bezick-purdue-university/pprl/runs/fj4za6pf")
+run6 = api.run("/michael-bezick-purdue-university/pprl/runs/knq0wkgz")
 
 list_of_runs = [run1, run2, run3, run4, run5, run6]
 
-plot_runs(list_of_runs=list_of_runs, key_x="_step", key_y="eval/DiscountedReturn", num_bootstrap_samples=1000, filename="large_mismatch_PCA.pdf")
+plot_runs(list_of_runs=list_of_runs, key_x="_step", key_y="eval/DiscountedReturn", num_bootstrap_samples=1000, filename="large_mismatch_no_aug.pdf")
