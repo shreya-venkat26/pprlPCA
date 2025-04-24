df115eae43054b43270b9f152a502f3bfb41a8ed
michaelBezick <mbezick@purdue.edu>
2025-04-24 15:29:25 -0400

    plots

--- plotting/bootstrapping.py
+++ plotting/bootstrapping.py
@@ -5,12 +5,12 @@ from functions import *
 
 api = wandb.Api()
 
-run1_string = "l68cj5gy"
-run2_string = "u10hzx4c"
-run3_string = "93vx0ko4"
-run4_string = "gtdrdpqj"
-run5_string = "a78goea7"
-run6_string = "qog0zrzf"
+run1_string = "5u0oy2dd"
+run2_string = "cgygtgtv"
+run3_string = "c70ewr80"
+run4_string = "hw6uwr0a"
+run5_string = "9orwioi4"
+run6_string = "nrshhq3b"
 
 # plotting 30mm 0 30mm pca
 
@@ -23,5 +23,5 @@ run6 = api.run("/michael-bezick-purdue-university/pprl/runs/" + run6_string)
 
 list_of_runs = [run1, run2, run3, run4, run5, run6]
 
-plot_runs(list_of_runs=list_of_runs, key_x="_step", key_y="eval/DiscountedReturn", num_bootstrap_samples=1000, filename="small_mismatch_no_aug_dis_ret.pdf")
-plot_runs(list_of_runs=list_of_runs, key_x="_step", key_y="eval/Success", num_bootstrap_samples=1000, filename="small_mismatch_no_aug_eval_success.pdf")
+plot_runs(list_of_runs=list_of_runs, key_x="_step", key_y="eval/DiscountedReturn", num_bootstrap_samples=1000, filename="small_mismatch_PCA_dis_ret.pdf")
+plot_runs(list_of_runs=list_of_runs, key_x="_step", key_y="eval/Success", num_bootstrap_samples=1000, filename="small_mismatch_PCA_eval_success.pdf")
