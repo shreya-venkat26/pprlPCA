d997b32d43cdfea93b22d569884381eb9015955a
michaelBezick <mbezick@purdue.edu>
2025-04-25 11:06:18 -0400

    plots

--- plotting/bootstrapping.py
+++ plotting/bootstrapping.py
@@ -1,27 +1,9 @@
 import wandb
 from functions import *
 
+group_name = "30_0_0mm_no_aug"
 
+list_of_runs = get_runs_by_group(group_name)
 
-api = wandb.Api()
-
-run1_string = "5u0oy2dd"
-run2_string = "cgygtgtv"
-run3_string = "c70ewr80"
-run4_string = "hw6uwr0a"
-run5_string = "9orwioi4"
-run6_string = "nrshhq3b"
-
-# plotting 30mm 0 30mm pca
-
-run1 = api.run("/michael-bezick-purdue-university/pprl/runs/" + run1_string)
-run2 = api.run("/michael-bezick-purdue-university/pprl/runs/" + run2_string)
-run3 = api.run("/michael-bezick-purdue-university/pprl/runs/" + run3_string)
-run4 = api.run("/michael-bezick-purdue-university/pprl/runs/" + run4_string)
-run5 = api.run("/michael-bezick-purdue-university/pprl/runs/" + run5_string)
-run6 = api.run("/michael-bezick-purdue-university/pprl/runs/" + run6_string)
-
-list_of_runs = [run1, run2, run3, run4, run5, run6]
-
-plot_runs(list_of_runs=list_of_runs, key_x="_step", key_y="eval/DiscountedReturn", num_bootstrap_samples=1000, filename="small_mismatch_PCA_dis_ret.pdf")
-plot_runs(list_of_runs=list_of_runs, key_x="_step", key_y="eval/Success", num_bootstrap_samples=1000, filename="small_mismatch_PCA_eval_success.pdf")
+plot_runs(list_of_runs=list_of_runs, key_x="_step", key_y="eval/DiscountedReturn", num_bootstrap_samples=1000, filename= group_name + "_dis_ret.pdf")
+plot_runs(list_of_runs=list_of_runs, key_x="_step", key_y="eval/Success", num_bootstrap_samples=1000, filename= group_name + "_eval_success.pdf")
--- plotting/functions.py
+++ plotting/functions.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import collections
+import wandb
+
+def get_runs_by_group(group_name, entity="michael-bezick-purdue-university", project="pprl"):
+    api = wandb.Api()
+    runs = api.runs(f"{entity}/{project}")
+    return [run for run in runs if run.group == group_name]
 
 def get_data(run, key1, key2):
 
@@ -65,4 +71,82 @@ def plot_runs(list_of_runs, key_x, key_y, num_bootstrap_samples, filename):
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
+    plt.savefig(filename)
+
+def plot_runs_comparison(list_of_runs1, list_of_runs2, key_x, key_y, num_bootstrap_samples, filename):
+
+    # key 2 will be y variable
+
+    x_to_ys = collections.defaultdict(list)
+    means, lowers, uppers = [], [], []
+
+
+    for run in list_of_runs1:
+        data_x, data_y = get_data(run, key_x, key_y)
+        for x, y in zip(data_x, data_y):
+            x_to_ys[x].append(y)
+
+    sorted_xs = sorted(x_to_ys.keys())
+
+    for x in sorted_xs:
+        y_values = x_to_ys[x]
+        mean = np.mean(y_values)
+        lower, upper = construct_bootstrapped_CI(y_values, num_bootstrap_samples)
+        means.append(mean)
+        lowers.append(lower)
+        uppers.append(upper)
+
+    # Convert to arrays for plotting
+    xs = np.array(sorted_xs)
+    means = np.array(means)
+    lowers = np.array(lowers)
+    uppers = np.array(uppers)
+
+
+    # Plot with confidence interval
+    plt.figure(figsize=(8, 5))
+    plt.plot(xs, means, label="Mean " + key_y)
+    plt.fill_between(xs, lowers, uppers, color='blue', alpha=0.3, label="95% CI")
+    plt.xlabel(key_x)
+    plt.ylabel(key_y)
+    # plt.title(f"{key_y} with 95% Bootstrap CI")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+
+    x_to_ys = collections.defaultdict(list)
+    means, lowers, uppers = [], [], []
+
+
+    for run in list_of_runs2:
+        data_x, data_y = get_data(run, key_x, key_y)
+        for x, y in zip(data_x, data_y):
+            x_to_ys[x].append(y)
+
+    sorted_xs = sorted(x_to_ys.keys())
+
+    for x in sorted_xs:
+        y_values = x_to_ys[x]
+        mean = np.mean(y_values)
+        lower, upper = construct_bootstrapped_CI(y_values, num_bootstrap_samples)
+        means.append(mean)
+        lowers.append(lower)
+        uppers.append(upper)
+
+    # Convert to arrays for plotting
+    xs = np.array(sorted_xs)
+    means = np.array(means)
+    lowers = np.array(lowers)
+    uppers = np.array(uppers)
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(xs, means, label="Mean " + key_y)
+    plt.fill_between(xs, lowers, uppers,  alpha=0.3, label="95% CI")
+    plt.xlabel(key_x)
+    plt.ylabel(key_y)
+    plt.title(f"{key_y} with 95% Bootstrap CI")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+
     plt.savefig(filename)
