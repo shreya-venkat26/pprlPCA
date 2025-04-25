580bd8bcb78caca96f05860b1c22429f158ade0a
michaelBezick <mbezick@purdue.edu>
2025-04-25 11:24:44 -0400

    comparison plots

--- plotting/bootstrapping.py
+++ plotting/bootstrapping.py
@@ -1,9 +1,7 @@
 import wandb
 from functions import *
 
-group_name = "30_0_0mm_no_aug"
+group_names = ["30_0_30mm_no_aug", "30_0_30mm_pca"]
 
-list_of_runs = get_runs_by_group(group_name)
-
-plot_runs(list_of_runs=list_of_runs, key_x="_step", key_y="eval/DiscountedReturn", num_bootstrap_samples=1000, filename= "plots/" + group_name + "_dis_ret.pdf")
-plot_runs(list_of_runs=list_of_runs, key_x="_step", key_y="eval/Success", num_bootstrap_samples=1000, filename= "plots/" + group_name + "_eval_success.pdf")
+plot_multiple(group_names, key_x="_step", key_y="eval/DiscountedReturn", group_labels=["No augmentation", "PCA Augmentation"], filename="30_0_30mm_comparison_dis_return.pdf")
+plot_multiple(group_names, key_x="_step", key_y="eval/Success", group_labels=["No augmentation", "PCA Augmentation"], filename="30_0_30mm_comparison_eval.pdf")
--- plotting/functions.py
+++ plotting/functions.py
@@ -6,6 +6,7 @@ import wandb
 def get_runs_by_group(group_name, entity="michael-bezick-purdue-university", project="pprl"):
     api = wandb.Api()
     runs = api.runs(f"{entity}/{project}")
+
     return [run for run in runs if run.group == group_name]
 
 def get_data(run, key1, key2):
@@ -73,80 +74,71 @@ def plot_runs(list_of_runs, key_x, key_y, num_bootstrap_samples, filename):
     plt.tight_layout()
     plt.savefig(filename)
 
-def plot_runs_comparison(list_of_runs1, list_of_runs2, key_x, key_y, num_bootstrap_samples, filename):
 
-    # key 2 will be y variable
+def plot_multiple(group_names, key_x, key_y, filename, group_labels, num_bootstrap_samples=1000):
+    list_of_list_of_runs = []
+    for group_name in group_names:
 
-    x_to_ys = collections.defaultdict(list)
-    means, lowers, uppers = [], [], []
+        list_of_runs = get_runs_by_group(group_name)
+        list_of_list_of_runs.append(list_of_runs)
 
+        if len(list_of_runs) == 0:
+            print("Invalid Group Name")
+            exit()
 
-    for run in list_of_runs1:
-        data_x, data_y = get_data(run, key_x, key_y)
-        for x, y in zip(data_x, data_y):
-            x_to_ys[x].append(y)
+    plot_multiple_run_groups(list_of_list_of_runs=list_of_list_of_runs, key_x=key_x, key_y=key_y, num_bootstrap_samples=1000, filename= "plots/" + filename, group_labels=group_labels)
 
-    sorted_xs = sorted(x_to_ys.keys())
+def plot_multiple_run_groups(list_of_list_of_runs, key_x, key_y, num_bootstrap_samples, filename, group_labels=None):
+    """
+    Plots multiple groups of runs on the same plot with confidence intervals.
+    
+    Parameters:
+        list_of_list_of_runs: List of groups, where each group is a list of runs.
+        key_x: Key for x-axis.
+        key_y: Key for y-axis.
+        num_bootstrap_samples: Number of bootstrap samples for CI.
+        filename: Output filename.
+        group_labels: Optional list of labels for each group.
+    """
+    plt.figure(figsize=(10, 6))
 
-    for x in sorted_xs:
-        y_values = x_to_ys[x]
-        mean = np.mean(y_values)
-        lower, upper = construct_bootstrapped_CI(y_values, num_bootstrap_samples)
-        means.append(mean)
-        lowers.append(lower)
-        uppers.append(upper)
+    for i, list_of_runs in enumerate(list_of_list_of_runs):
+        x_to_ys = collections.defaultdict(list)
 
-    # Convert to arrays for plotting
-    xs = np.array(sorted_xs)
-    means = np.array(means)
-    lowers = np.array(lowers)
-    uppers = np.array(uppers)
+        # Gather data from all runs in the group
+        for run in list_of_runs:
+            data_x, data_y = get_data(run, key_x, key_y)
+            for x, y in zip(data_x, data_y):
+                x_to_ys[x].append(y)
 
+        # Prepare statistics
+        sorted_xs = sorted(x_to_ys.keys())
+        means, lowers, uppers = [], [], []
 
-    # Plot with confidence interval
-    plt.figure(figsize=(8, 5))
-    plt.plot(xs, means, label="Mean " + key_y)
-    plt.fill_between(xs, lowers, uppers, color='blue', alpha=0.3, label="95% CI")
+        for x in sorted_xs:
+            y_values = x_to_ys[x]
+            mean = np.mean(y_values)
+            lower, upper = construct_bootstrapped_CI(y_values, num_bootstrap_samples)
+            means.append(mean)
+            lowers.append(lower)
+            uppers.append(upper)
+
+        # Convert to arrays for plotting
+        xs = np.array(sorted_xs)
+        means = np.array(means)
+        lowers = np.array(lowers)
+        uppers = np.array(uppers)
+
+        label = group_labels[i] if group_labels is not None else f"Group {i+1}"
+
+        # Plot line and confidence interval
+        plt.plot(xs, means, label=label)
+        plt.fill_between(xs, lowers, uppers, alpha=0.3)
+
     plt.xlabel(key_x)
     plt.ylabel(key_y)
-    # plt.title(f"{key_y} with 95% Bootstrap CI")
+    plt.title(f"{key_y} Comparison with 95% Bootstrap CI")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
-
-    x_to_ys = collections.defaultdict(list)
-    means, lowers, uppers = [], [], []
-
-
-    for run in list_of_runs2:
-        data_x, data_y = get_data(run, key_x, key_y)
-        for x, y in zip(data_x, data_y):
-            x_to_ys[x].append(y)
-
-    sorted_xs = sorted(x_to_ys.keys())
-
-    for x in sorted_xs:
-        y_values = x_to_ys[x]
-        mean = np.mean(y_values)
-        lower, upper = construct_bootstrapped_CI(y_values, num_bootstrap_samples)
-        means.append(mean)
-        lowers.append(lower)
-        uppers.append(upper)
-
-    # Convert to arrays for plotting
-    xs = np.array(sorted_xs)
-    means = np.array(means)
-    lowers = np.array(lowers)
-    uppers = np.array(uppers)
-
-    plt.figure(figsize=(8, 5))
-    plt.plot(xs, means, label="Mean " + key_y)
-    plt.fill_between(xs, lowers, uppers,  alpha=0.3, label="95% CI")
-    plt.xlabel(key_x)
-    plt.ylabel(key_y)
-    plt.title(f"{key_y} with 95% Bootstrap CI")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-
     plt.savefig(filename)
